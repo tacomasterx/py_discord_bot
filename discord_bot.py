cf6f0dc75f180d06811d472b1279ace39765b888
Tomas Carrillo <tacoman8918@gmail.com>
2021-08-16 14:56:05 -0500

    First commit with working bot and env variables

---
+++ discord_bot.py
@@ -0,0 +1,44 @@
+import discord
+import os
+channel = int(os.environ['DISCORDCHANNELS'])
+
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def hello(ctx):
+    await bot.get_channel(channel).send("Mensaje de prueba :3")
+
+@bot.command()
+async def algo(ctx):
+    await bot.get_channel(channel).send("Otra cosa")
+
+@bot.command()
+async def insulto(ctx):
+    await bot.get_channel(channel).send("Gorzed es puto!")
+
+@bot.command()
+async def texto(ctx, string):
+    await bot.get_channel(channel).send("No se que signifique \"" + string + "\"")
+
+@bot.command()
+async def potencia(ctx, base, exponente):
+    total = 1
+    base = int(base)
+    exponente = int(exponente)
+    for i in range(exponente):
+        total = total * base
+    #string = "La {}a potencia de {} es {}.".format(exponente,base,total)
+    await bot.get_channel(channel).send("La {}a potencia de {} es {}.".format(exponente,base,total))
+
+@bot.command()
+async def factorial(ctx, base):
+    base = int(base)
+    total = base
+    for i in range(1,base):
+        total = total * (base - i)
+    #string = "La {}a potencia de {} es {}.".format(exponente,base,total)
+    await bot.get_channel(channel).send("El factorial de {} es {}.".format(base,total))
+
+bot.run(os.environ['TDBTOKEN'])
---
+++ test.py
@@ -0,0 +1,17 @@
+# exponente = 3
+# base = 3
+# def factorial(base):
+#     total = base
+#     for i in range(1,base):
+#         total = total * (base - i)
+#     return(total)
+# print(factorial(11))
+# def potencia(base, exponente):
+#     total = 1
+#     for i in range(exponente):
+#         total = total * base
+#     return total 
+# print("La " + str(exponente) + "a potencia de " + str(base) + " es " + str(potencia(base, exponente)) )
+
+def lineal(sring):
+    print(string)
