0c11e14509fec8c9a707101a258aa352f80e2131
Tomas Carrillo <tacoman8918@gmail.com>
2021-08-16 21:25:21 -0500

    Added other commands   for linear equations

--- discord_bot.py
+++ discord_bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import numpy as np
               # Variable de entorno para el id del canal/canales
 channel = int(os.environ['DISCORDCHANNELS'])
 
@@ -41,6 +42,90 @@ async def factorial(ctx, base):
         total = total * (base - i)
     #string = "La {}a potencia de {} es {}.".format(exponente,base,total)
     await bot.get_channel(channel).send("El factorial de {} es {}.".format(base,total))
+
+@bot.command()
+async def lineal(ctx, equation):
+    members = equation.split("=")
+    message = ""
+    if len(members) == 2 and members[1].isnumeric():
+        terms = []
+        buffer = ""
+        for i in range(len(members[0])):
+            if i == 0 and members[0][i] == '-':
+                buffer += members[0][i]
+                continue
+            if i != 0 and members[0][i] == '+':
+                terms.append(int(buffer))
+                buffer = ""
+                continue
+            if i != 0 and members[0][i] == '-':
+                terms.append(int(buffer))
+                buffer = "-"
+                continue
+            if members[0][i].isnumeric() and i < len(members[0]) - 1:
+                buffer += members[0][i]
+                continue
+            if i == len(members[0]) - 1:
+                if members[0][i].isnumeric():
+                    buffer += members[0][i]
+                terms.append(int(buffer))
+                break
+
+        A = np.array(terms)
+        B = np.array([int(members[1])])
+        x = ((A[1] * -1) + B[0]) / A[0]
+        message = "El valor de x es: " + str(x)
+    else:
+        message = "Arregla tu ecuación {} prro".format(equation)
+    await bot.get_channel(channel).send(message)
+
+
+@bot.command()
+async def sistemal(ctx, equation1, equation2):
+    equations = [equation1, equation2]
+    message = ""
+    term_list = []
+    member = []
+    member_list = []
+    for j in range(len(equations)):
+        terms = []
+        buffer = ""
+        members = equations[j].split("=")
+        if len(members) == 2 and members[1].isnumeric():
+            member = int(members[1])
+            member_list.append(member)
+            for i in range(len(members[0])):
+                if i == 0 and members[0][i] == '-':
+                    buffer += members[0][i]
+                    continue
+                if i != 0 and members[0][i] == '+':
+                    terms.append(int(buffer))
+                    buffer = ""
+                    continue
+                if i != 0 and members[0][i] == '-':
+                    terms.append(int(buffer))
+                    buffer = "-"
+                    continue
+                if members[0][i].isnumeric() and i < len(members[0]) - 1:
+                    buffer += members[0][i]
+                    continue
+                if i == len(members[0]) - 1:
+                    if members[0][i].isnumeric():
+                        buffer += members[0][i]
+                    terms.append(int(buffer))
+                    break
+        else:
+            message = "Arregla tu ecuación {} prro".format(j+1)
+            await bot.get_channel(channel).send(message)
+            return
+
+        term_list.append(terms)
+
+    A = np.array(term_list)
+    B = np.array(member_list)
+    X = np.linalg.inv(A).dot(B)
+    message = "Los valores de las incógnitas son: {}".format(X)
+    await bot.get_channel(channel).send(message)
 
         # Variable de entorno para el token del bot
 bot.run(os.environ['TDBTOKEN'])
--- test.py
+++ test.py
@@ -1,17 +1,95 @@
-# exponente = 3
-# base = 3
-# def factorial(base):
-#     total = base
-#     for i in range(1,base):
-#         total = total * (base - i)
-#     return(total)
-# print(factorial(11))
-# def potencia(base, exponente):
-#     total = 1
-#     for i in range(exponente):
-#         total = total * base
-#     return total 
-# print("La " + str(exponente) + "a potencia de " + str(base) + " es " + str(potencia(base, exponente)) )
+import numpy as np
+# function: takes a simple linear equation as a string and returns
+#           the literal value
+def lineal(equation):
+    members = equation.split("=")
+    message = ""
+    if len(members) == 2 and members[1].isnumeric():
+        terms = []
+        buffer = ""
+        for i in range(len(members[0])):
+            if i == 0 and members[0][i] == '-':
+                buffer += members[0][i]
+                continue
+            if i != 0 and members[0][i] == '+' :
+                terms.append(int(buffer))
+                buffer = ""
+                continue
+            if i != 0 and members[0][i] == '-':
+                terms.append(int(buffer))
+                buffer = "-"
+                continue
+            if members[0][i].isnumeric() and i < len(members[0]) - 1:
+                buffer += members[0][i]
+                continue
+            if i == len(members[0]) - 1:
+                if members[0][i].isnumeric():
+                    buffer += members[0][i]
+                terms.append(int(buffer))
+                break
 
-def lineal(sring):
-    print(string)
+        A = np.array(terms)
+        B = np.array([int(members[1])])
+        x = (( A[1] * -1 ) + B[0] )/ A[0]
+        message = "El valor de x es: " + str(x)
+    else:
+        message = "Arregla tu ecuación {} prro".format(equation)
+    return message
+
+
+def sistema(equation1, equation2):   
+    equations = [equation1,equation2]
+    message = ""
+    term_list = []
+    member = []
+    member_list = []
+    for j in range(len(equations)):
+        terms = []
+        buffer = ""
+        members = equations[j].split("=")
+        if len(members) == 2 and members[1].isnumeric():
+            member = int(members[1])
+            member_list.append(member)
+            for i in range(len(members[0])):
+                if i == 0 and members[0][i] == '-':
+                    buffer += members[0][i]
+                    continue
+                if i != 0 and members[0][i] == '+':
+                    terms.append(int(buffer))
+                    buffer = ""
+                    continue
+                if i != 0 and members[0][i] == '-':
+                    terms.append(int(buffer))
+                    buffer = "-"
+                    continue
+                if members[0][i].isnumeric() and i < len(members[0]) - 1:
+                    buffer += members[0][i]
+                    continue
+                if i == len(members[0]) - 1:
+                    if members[0][i].isnumeric():
+                        buffer += members[0][i]
+                    terms.append(int(buffer))
+                    break            
+        else:
+            message = "Arregla tu ecuación {} prro".format(j+1)
+            return message
+
+        term_list.append(terms)
+
+
+
+    A = np.array(term_list)
+    B = np.array(member_list)
+    X = np.linalg.inv(A).dot(B)
+    message = "Los valores de las incógnitas son: {}".format(X)
+
+
+    print(A)
+    print(B)
+    print(message)
+    print( -32 * (-0.81818182) + 15 * (-1.54545455))
+    print(  13 * (-0.81818182) - 25 * (-1.54545455))
+    
+
+print(lineal("-32x+15=3"))
+print(sistema("-32x+15y=3", "13x-25y=28"))
